24b2f6f982e2600cf04144a415b6fc805eaff165
unknown <ashylnikov@P000066.playtika.local>
2013-09-03 10:40:20 +0300

    1st commit

---
+++ Napoli/MainForm.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Сделано в SharpDevelop.
+ * Пользователь: ashylnikov
+ * Дата: 03.09.2013
+ * Время: 10:38
+ * 
+ * Для изменения этого шаблона используйте Сервис | Настройка | Кодирование | Правка стандартных заголовков.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Napoli
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
---
+++ Napoli/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Сделано в SharpDevelop.
+ * Пользователь: ashylnikov
+ * Дата: 03.09.2013
+ * Время: 10:38
+ * 
+ * Для изменения этого шаблона используйте Сервис | Настройка | Кодирование | Правка стандартных заголовков.
+ */
+using System;
+using System.Windows.Forms;
+
+namespace Napoli
+{
+	/// <summary>
+	/// Class with program entry point.
+	/// </summary>
+	internal sealed class Program
+	{
+		/// <summary>
+		/// Program entry point.
+		/// </summary>
+		[STAThread]
+		private static void Main(string[] args)
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new MainForm());
+		}
+		
+	}
+}
